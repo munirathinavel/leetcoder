4cc5abbb8a7b96ed5c98d08e5e9b8c6ff870672f
munirathinavel <munirathinavel@gmail.com>
2022-03-12 23:30:36 +0530

    Time: 119 ms, Memory: 117.2 MB - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,9 +1,19 @@
 class Solution {
+    int[] memo;
     public int fib(int n) {
+        memo = new int[n];
+        return fib(n, memo);
+    }
+    
+    private int fib(int n, int[] memo) {
         if(n < 2) {
             return n;
+        } else if(memo[n-1] > 0 ) {
+            return memo[n-1];
+        } else {
+            int res = fib(n-1) + fib(n-2);
+            memo[n-1] = res;
+            return res;
         }
-        
-        return fib(n-1) + fib( n-2);
     }
 }
