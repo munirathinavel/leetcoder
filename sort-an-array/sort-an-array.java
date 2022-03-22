7ff4c495c35f32abee107c2d27cc671d696d59c8
munirathinavel <munirathinavel@gmail.com>
2022-03-22 09:33:52 +0530

    Time: 10 ms, Memory: 51.3 MB - LeetHub

--- sort-an-array/sort-an-array.java
+++ sort-an-array/sort-an-array.java
@@ -1,27 +1,29 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        if (nums == null || nums.length == 0) return nums;
-        mergeSort(nums, 0, nums.length - 1);
+       sortArray(nums, 0, nums.length-1);
         return nums;
     }
-    private void mergeSort(int[] nums, int l, int r) {
-        if (l >= r) return;
-        int mid = l + (r - l) / 2;
-        mergeSort(nums, l, mid);
-        mergeSort(nums, mid + 1, r);
+    
+    private void sortArray(int[] nums, int l, int r) {
+        if(l >= r) return;
+        int mid = l + (r-l) / 2;
+        sortArray(nums, l, mid);
+        sortArray(nums, mid+1, r);
         merge(nums, l, r);
     }
+    
     private void merge(int[] nums, int l, int r) {
         int mid = l + (r - l) / 2;
-        int[] tmp = new int[r - l + 1];
-        int i = l, j = mid + 1, k = 0;
-        while (i <= mid || j <= r) {
-            if (i > mid || j <= r && nums[i] > nums[j]) {
-                tmp[k++] = nums[j++];
+        int[] temp = new int[r-l+1];
+        int lc = l, rc = mid + 1, tc = 0;
+        while(lc <= mid || rc <= r) {
+            if(lc > mid || rc <= r && nums[lc] > nums[rc]) {
+                temp[tc++] = nums[rc++];
             } else {
-                tmp[k++] = nums[i++];
+                temp[tc++] = nums[lc++];
             }
         }
-        System.arraycopy(tmp, 0, nums, l, r - l + 1);
+        System.arraycopy(temp, 0, nums, l, r-l+1);
     }
+   
 }
