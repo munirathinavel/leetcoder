4abd6712a3a42d406c22c01de0cdd8e61a5f7932
munirathinavel <munirathinavel@gmail.com>
2022-03-24 09:30:37 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (85.29%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,8 +15,40 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (!check(p, q)) return false;
+        LinkedList<TreeNode> pList = new LinkedList<>();
+        LinkedList<TreeNode> qList = new LinkedList<>();
+        pList.add(p);
+        qList.add(q);
+        while(!pList.isEmpty()) {
+            TreeNode p1 = pList.removeFirst();
+            TreeNode q1 = qList.removeFirst();
+            if(!check(p1, q1)) {
+                return false;
+            }
+            if(p1 != null) {
+                if(!check(p1.left, q1.left)) {
+                    return false;
+                }
+                if(p.left != null) {
+                    pList.add(p1.left);
+                    qList.add(q1.left);
+                }
+                if(!check(p1.right, q1.right)) {
+                    return false;
+                }
+                if(p.right != null) {
+                    pList.add(p1.right);
+                    qList.add(q1.right);
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean check(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
-        if(p == null || q == null || p.val != q.val)return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if(p == null || q == null || p.val != q.val) return false;
+        return true;
     }
 }
