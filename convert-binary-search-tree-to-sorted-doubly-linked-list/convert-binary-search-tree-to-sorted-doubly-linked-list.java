fb4ad97db31b219a1aac1ae735a30fd064411701
munirathinavel <munirathinavel@gmail.com>
2022-03-24 11:25:16 +0530

    Time: 0 ms, Memory: 42.8 MB - LeetHub

---
+++ convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    Node last = null;
+    Node first = null;
+    
+    public void helper(Node node){
+        if(node == null) return;
+        helper(node.left);
+        if(last != null) {
+           last.right = node;
+            node.left = last;
+        } else {
+            first = node;
+        }
+        last = node;
+        helper(node.right);
+    }
+    
+   
+    
+    public Node treeToDoublyList(Node root) {
+        if(root == null)
+            return null;
+        
+        helper(root);
+        last.right = first;
+        first.left = last;
+        return first;
+    }
+}
