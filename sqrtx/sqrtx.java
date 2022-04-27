265bc471013d8b4485c78ffa68af3aff9dd8f0cc
munirathinavel <munirathinavel@gmail.com>
2022-04-27 12:24:24 +0530

    Time: 3 ms, Memory: 41.2 MB - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -1,32 +1,10 @@
-// class Solution {
-//     public int mySqrt(int x) {
-//         if(x < 2) {
-//             return x;
-//         }
-//         long num;
-//         int l = 2, r = x/2;
-//         while(l <= r) {
-//             int mid = l + (r-l)/2;
-//             num = (long)mid*mid;
-//             if(num > x) {
-//                 r = mid -1;
-//             } else if(num < x) {
-//                 l = mid + 1;
-//             } else {
-//                 return mid;
-//             }
-//         }
-//         return r;
-        
-//     }
-// }
 
 class Solution {
     public int mySqrt(int x) {
         if(x < 2) {
             return x;
         }
-        int l = 2, r = x/2;
+        int l = 0, r = x/2;
         while(l <=r) {
             int mid = l + (r-l) /2;
             long square = (long)mid * mid;
