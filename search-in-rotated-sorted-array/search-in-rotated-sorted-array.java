ef6d60bb50aad1f23f4ab9a6430ea9be246d6d47
munirathinavel <munirathinavel@gmail.com>
2022-04-27 13:58:30 +0530

    Time: 1 ms, Memory: 43.2 MB - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,22 +1,48 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int l = 0;
+//         int r = nums.length -1;
+//         while(l <= r) {
+//             int mid = l + (r-l)/2;
+//             if(nums[mid] == target) {
+//                 return mid;
+//             } else if(nums[l] <= nums[mid]) {
+//                 if(nums[l] <= target && nums[mid] > target) {
+//                     r = mid -1;
+//                 } else {
+//                     l = mid + 1;
+//                 }
+//             } else {
+//                 if(nums[r] >= target && nums[mid] < target) {
+//                     l = mid +1;
+//                 } else {
+//                     r = mid -1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length -1;
+       int l = 0, r = nums.length -1;
         while(l <= r) {
-            int mid = l + (r-l)/2;
+            int mid = l + (r - l) / 2;
             if(nums[mid] == target) {
                 return mid;
-            } else if(nums[l] <= nums[mid]) {
+            }
+            else if(nums[l] <= nums[mid]) {
                 if(nums[l] <= target && nums[mid] > target) {
-                    r = mid -1;
+                    r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
             } else {
                 if(nums[r] >= target && nums[mid] < target) {
-                    l = mid +1;
+                    l = mid + 1;
                 } else {
-                    r = mid -1;
+                    r = mid - 1;
                 }
             }
         }
