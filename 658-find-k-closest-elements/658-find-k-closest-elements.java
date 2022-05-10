e739b8320c982b1aaac6807dd022f097c73820fa
munirathinavel <munirathinavel@gmail.com>
2022-05-10 10:52:12 +0530

    Time: 7 ms (52.64%), Space: 63.1 MB (20.16%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,32 +1,32 @@
 // Time Complexity - O(N.log(N) + k.log(k))
-//Space Complexity
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> sortedArray = new ArrayList<>();
-        for(int num: arr) {
-            sortedArray.add(num);
-        }
-        Collections.sort(sortedArray, (n1, n2) -> Math.abs(n1-x) - Math.abs(n2 - x));
-        sortedArray = sortedArray.subList(0,k);
-        Collections.sort(sortedArray);
-        return sortedArray;
-    }
-}
+//Space Complexity = O(N)
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//         List<Integer> sortedArray = new ArrayList<>();
+//         for(int num: arr) {
+//             sortedArray.add(num);
+//         }
+//         Collections.sort(sortedArray, (n1, n2) -> Math.abs(n1-x) - Math.abs(n2 - x));
+//         sortedArray = sortedArray.subList(0,k);
+//         Collections.sort(sortedArray);
+//         return sortedArray;
+//     }
+// }
 
 
 // Time Complexity - O(log(N-k) + k)
-//Space Complexity - 1
-// class Solution {
-//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-//         int l = 0, r = arr.length - k;
-//         while(l < r) {
-//             int mid = l + (r-l) / 2;
-//             if(x - arr[mid] > arr[mid+k] -x) {
-//                 l = mid + 1;
-//             } else {
-//                 r = mid;
-//             }
-//         }
-//         return Arrays.stream(arr, l, l+k).boxed().collect(Collectors.toList());
-//     }
-// }
+// Space Complexity - 1
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int l = 0, r = arr.length - k;
+        while(l < r) {
+            int mid = l + (r-l) / 2;
+            if(x - arr[mid] > arr[mid+k] -x) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return Arrays.stream(arr, l, l+k).boxed().collect(Collectors.toList());
+    }
+}
