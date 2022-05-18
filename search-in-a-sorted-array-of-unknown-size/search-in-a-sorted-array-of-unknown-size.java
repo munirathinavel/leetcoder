ce089e31fc6847e644962594760400e1f62d2962
munirathinavel <munirathinavel@gmail.com>
2022-05-18 11:22:45 +0530

    Time: 4 ms, Memory: 54.8 MB - LeetHub

--- search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.java
+++ search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.java
@@ -7,15 +7,35 @@
  */
 
 class Solution {
+    // public int search(ArrayReader reader, int target) {
+    //     int l = 0, r = Integer.MAX_VALUE;
+    //     while(l <= r) {
+    //         int mid = l + (r-l) / 2;
+    //         int val = reader.get(mid);
+    //         // System.out.println("val=" + val + ", left=" + l + ", mid=" + mid  + ", right=" + r);
+    //         if( val == target) {
+    //             return mid;
+    //         } else if(val == Integer.MAX_VALUE || val > target) {
+    //             r = mid - 1;
+    //         } else {
+    //             l = mid + 1;
+    //         }
+    //     }
+    //     return -1;
+    // }
     public int search(ArrayReader reader, int target) {
-        int l = 0, r = Integer.MAX_VALUE;
+        if(reader.get(0) == target) return 0;
+        int l = 0, r = 1;
+        while(reader.get(r) < target) {
+            l = r;
+            r = r * 2;
+        }
         while(l <= r) {
             int mid = l + (r-l) / 2;
             int val = reader.get(mid);
-            // System.out.println("val=" + val + ", left=" + l + ", mid=" + mid  + ", right=" + r);
             if( val == target) {
                 return mid;
-            } else if(val == Integer.MAX_VALUE || val > target) {
+            } else if(val > target) {
                 r = mid - 1;
             } else {
                 l = mid + 1;
