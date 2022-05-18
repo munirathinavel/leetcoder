eef90ec6f949fb8290e62f026a966488264f1db7
munirathinavel <munirathinavel@gmail.com>
2022-05-19 00:09:51 +0530

    Time: 1 ms, Memory: 41 MB - LeetHub

---
+++ valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num < 2) {
+            return true;
+        }
+        long l = 2, r = num /2;
+        while(l <= r) {
+            long  x = l+(r-l)/2;
+           long  guess = x * x;
+            if(guess == num) {
+                return true; 
+            } else if(guess > num) {
+                 r = x - 1;
+            } else {
+               l = x + 1;    
+            }
+        }
+        return false;
+    }
+}
