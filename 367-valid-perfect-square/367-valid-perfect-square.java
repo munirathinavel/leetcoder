440621c6afbc0f9b9dfa6cb299eb148b9b17c516
munirathinavel <munirathinavel@gmail.com>
2022-05-19 00:09:15 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (52.49%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -9,10 +9,10 @@ class Solution {
            long  guess = x * x;
             if(guess == num) {
                 return true; 
-            } else if(guess < num) {
-                l = x + 1;    
+            } else if(guess > num) {
+                 r = x - 1;
             } else {
-                r = x - 1;
+               l = x + 1;    
             }
         }
         return false;
