3fcfbde78b718fffe2ebdbbdffc477ed6429a1cb
munirathinavel <munirathinavel@gmail.com>
2022-05-19 07:39:12 +0530

    Time: 1 ms, Memory: 43.3 MB - LeetHub

--- powx-n/powx-n.java
+++ powx-n/powx-n.java
@@ -1,18 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
+        if(n == 0) {
+            return 1.0;
+        }
         long N = n;
-        if (N < 0) {
-            x = 1 / x;
+        if(N < 0) {
             N = -N;
+            x = 1/x;
         }
         double ans = 1;
-        double currentProduct = x;
-        for (long i = N; i > 0; i = i/2) {
-            if(i % 2 == 1) {
-                ans = ans * currentProduct;
+        double currentAns = x;
+        for(long i = N; i > 0; i = i/2) {
+            if(i %2 == 1) {
+                ans = ans * currentAns;
             }
-            currentProduct = currentProduct * currentProduct;
+            currentAns = currentAns * currentAns;
         }
         return ans;
     }
 };
+
+// 
