64ec47f0f191eefc4afb267cc6202d035c2aa662
munirathinavel <munirathinavel@gmail.com>
2022-05-25 09:33:07 +0530

    Time: 0 ms, Memory: 51.5 MB - LeetHub

--- search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -13,17 +13,33 @@
  *     }
  * }
  */
+//  Recursive Solution
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-       while(root != null) {
-           if(val == root.val) {
-               return root;
-           } else if(val < root.val) {
-               root = root.left; 
-           } else {
-               root = root.right;
-           }
-       }
-        return null;
+        if(root == null) {
+            return null;
+        }
+        if(root.val == val) {
+            return root;
+        } else if(root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+           return searchBST(root.right, val);
+        }
     }
 }
+//  Iterative Solution
+// class Solution {
+//     public TreeNode searchBST(TreeNode root, int val) {
+//        while(root != null) {
+//            if(val == root.val) {
+//                return root;
+//            } else if(val < root.val) {
+//                root = root.left; 
+//            } else {
+//                root = root.right;
+//            }
+//        }
+//         return null;
+//     }
+// }
