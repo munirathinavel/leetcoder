1a698758460981c7b1c8e92d95ee81173a7deffd
munirathinavel <munirathinavel@gmail.com>
2022-05-25 10:21:29 +0530

    Time: 1 ms (100.00%), Space: 53.8 MB (58.77%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -13,26 +13,44 @@
  *     }
  * }
  */
-class Solution {
-    public TreeNode insertIntoBST(TreeNode root, int val) {
-        TreeNode node = root;
-        while(node != null) {
-            if(node.val > val) {
-                if(node.left == null) {
-                    node.left = new TreeNode(val);
-                    return root;
-                } else {
-                    node = node.left;
-                }
-            } else {
-                if(node.right == null) {
-                    node.right = new TreeNode(val);
-                    return root;
-                } else {
-                    node = node.right;
-                }
+// Recursive Solution
+    class Solution {
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+            if(root == null) {
+                return new TreeNode(val);
             }
+            // insert into left sub tree
+            if(root.val > val) {
+                root.left = insertIntoBST(root.left, val);
+            } else {
+                // insert into right sub tree
+                root.right = insertIntoBST(root.right, val);
+            }
+            return root;
         }
-        return new TreeNode(val);
     }
-}
+
+// Iterative Solution
+// class Solution {
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+//         TreeNode node = root;
+//         while(node != null) {
+//             if(node.val > val) {
+//                 if(node.left == null) {
+//                     node.left = new TreeNode(val);
+//                     return root;
+//                 } else {
+//                     node = node.left;
+//                 }
+//             } else {
+//                 if(node.right == null) {
+//                     node.right = new TreeNode(val);
+//                     return root;
+//                 } else {
+//                     node = node.right;
+//                 }
+//             }
+//         }
+//         return new TreeNode(val);
+//     }
+// }
