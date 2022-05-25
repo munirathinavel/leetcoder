dc62eda96e8b6e9291b9f9ab95f59cf4920b45a0
munirathinavel <munirathinavel@gmail.com>
2022-05-25 14:10:34 +0530

    Time: 4 ms (100.00%), Space: 43.2 MB (90.11%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -7,18 +7,37 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
+// Iterative Approach
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int parentVal = root.val;
         int pVal = p.val;
         int qVal = q.val;
-        if(pVal > parentVal && qVal > parentVal) {
-            return lowestCommonAncestor(root.right, p, q);
-        } else if(pVal < parentVal && qVal < parentVal) {
-            return lowestCommonAncestor(root.left, p, q);
-        } else {
-            return root;
+        TreeNode node = root;
+        while(node != null) {
+            int parentVal = node.val;
+            if(pVal > parentVal && qVal > parentVal) {
+                node = node.right;
+            } else if(pVal < parentVal && qVal < parentVal) {
+                node = node.left;
+            } else {
+                return node;
+            }
         }
+        return null;
     }
 }
+// Recursive Approach
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         int parentVal = root.val;
+//         int pVal = p.val;
+//         int qVal = q.val;
+//         if(pVal > parentVal && qVal > parentVal) {
+//             return lowestCommonAncestor(root.right, p, q);
+//         } else if(pVal < parentVal && qVal < parentVal) {
+//             return lowestCommonAncestor(root.left, p, q);
+//         } else {
+//             return root;
+//         }
+//     }
+// }
