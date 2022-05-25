f19dd1a2dddee1a8122fe09d94526e5e707f2558
munirathinavel <munirathinavel@gmail.com>
2022-05-25 09:23:12 +0530

    Time: 37 ms (6.74%), Space: 52.1 MB (24.26%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -14,31 +14,60 @@
  * }
  */
 class BSTIterator {
-    int index;
-    ArrayList<Integer> list;
+    Stack<TreeNode> stack;
     public BSTIterator(TreeNode root) {
-        list = new ArrayList<>();
-        index = -1;
-        inOrderTraversal(root);
+        stack = new Stack<TreeNode>();
+        leftMostInOrder(root);
+    }
+    private void leftMostInOrder(TreeNode root) {
+        while(root != null) {
+            stack.push(root);
+            root = root.left;
+        }
     }
     
     public int next() {
-        return list.get(++index);
+        TreeNode next = stack.pop();
+        TreeNode right = next.right;
+        if(right != null) {
+           leftMostInOrder(right);
+        }
+        return next.val;
     }
     
     public boolean hasNext() {
-        return index + 1 < list.size();
+        return !stack.isEmpty();
     }
     
-    private void inOrderTraversal(TreeNode root) {
-        if(root == null) {
-            return;
-        }
-        inOrderTraversal(root.left);
-        list.add(root.val);
-        inOrderTraversal(root.right);
-    }
+    
 }
+// Store all values in List while initiazing the object
+// class BSTIterator {
+//     int index;
+//     ArrayList<Integer> list;
+//     public BSTIterator(TreeNode root) {
+//         list = new ArrayList<>();
+//         index = -1;
+//         inOrderTraversal(root);
+//     }
+    
+//     public int next() {
+//         return list.get(++index);
+//     }
+    
+//     public boolean hasNext() {
+//         return index + 1 < list.size();
+//     }
+    
+//     private void inOrderTraversal(TreeNode root) {
+//         if(root == null) {
+//             return;
+//         }
+//         inOrderTraversal(root.left);
+//         list.add(root.val);
+//         inOrderTraversal(root.right);
+//     }
+// }
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
