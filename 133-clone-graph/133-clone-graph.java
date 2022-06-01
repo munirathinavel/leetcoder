21b50d9872670b3430b00ca25ba9f05f49694df2
munirathinavel <munirathinavel@gmail.com>
2022-06-01 12:56:25 +0530

    Time: 25 ms (94.61%), Space: 42.5 MB (82.36%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -17,28 +17,49 @@ class Node {
     }
 }
 */
-
+// DFS - Recursive
 class Solution {
+    HashMap<Node, Node> visited = new HashMap<>();
     public Node cloneGraph(Node node) {
         if(node == null) {
             return node;
         }
-        HashMap<Node, Node> visited = new HashMap();
-        LinkedList<Node> q = new LinkedList<Node>();
-        q.add(node);
-        visited.put(node, new Node(node.val, new ArrayList()));
-        
-        while(!q.isEmpty()) {
-            Node current = q.remove();
-            
-            for(Node neighbor: current.neighbors) {
-                if(!visited.containsKey(neighbor)) {
-                    visited.put(neighbor,new Node(neighbor.val, new ArrayList<>()));
-                    q.add(neighbor);
-                }
-                visited.get(current).neighbors.add(visited.get(neighbor));
-            }
+        if(visited.containsKey(node)) {
+            return visited.get(node);
         }
-        return visited.get(node);
+        Node clonedNode = new Node(node.val, new ArrayList());
+        visited.put(node, clonedNode);
+        
+        for(Node neighbor:node.neighbors) {
+            clonedNode.neighbors.add(cloneGraph(neighbor));
+        }
+        
+        return clonedNode;
     }
 }
+
+// BFS - Iterative
+// class Solution {
+//     public Node cloneGraph(Node node) {
+//         if(node == null) {
+//             return node;
+//         }
+//         HashMap<Node, Node> visited = new HashMap();
+//         LinkedList<Node> q = new LinkedList<Node>();
+//         q.add(node);
+//         visited.put(node, new Node(node.val, new ArrayList()));
+        
+//         while(!q.isEmpty()) {
+//             Node current = q.remove();
+            
+//             for(Node neighbor: current.neighbors) {
+//                 if(!visited.containsKey(neighbor)) {
+//                     visited.put(neighbor,new Node(neighbor.val, new ArrayList<>()));
+//                     q.add(neighbor);
+//                 }
+//                 visited.get(current).neighbors.add(visited.get(neighbor));
+//             }
+//         }
+//         return visited.get(node);
+//     }
+// }
