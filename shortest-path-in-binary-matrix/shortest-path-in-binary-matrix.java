d7afe22ca827416a64e3c5632763f6b0f58d7e9f
munirathinavel <munirathinavel@gmail.com>
2022-06-01 21:43:04 +0530

    Time: 52 ms, Memory: 72.1 MB - LeetHub

---
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    private static final int[][] directions = 
+        new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if(grid[0][0] != 0 || grid[m-1][n-1] != 0) {
+            return -1;
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+        grid[0][0] = 1;
+        
+        while(!q.isEmpty()) {
+            int[] cell = q.remove();
+            int row = cell[0];
+            int col = cell[1];
+            int distance = grid[row][col];
+            if(row == m-1 && col == m-1) {
+                return distance;
+            }
+            for(int[] neighbor: getNeighbors(row, col, grid)) {
+                int nRow = neighbor[0];
+                int nCol = neighbor[1];
+                q.add(new int[]{nRow, nCol});
+                grid[nRow][nCol] = distance + 1;
+            }
+        }
+        return -1;
+    }
+    
+    private List<int[]> getNeighbors(int row, int col, int[][] grid){
+        List<int[]> neighbors = new ArrayList<>();
+        for(int i =0; i < directions.length; i++) {
+            int newRow = row + directions[i][0];
+            int newCol = col + directions[i][1];
+            if(newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length || grid[newRow][newCol] != 0) {
+                continue;
+            }
+            neighbors.add(new int[]{newRow, newCol});
+        }
+        return neighbors;
+    }
+}
