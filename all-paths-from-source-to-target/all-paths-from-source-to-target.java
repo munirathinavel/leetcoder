7405ce0627320f8c9cbe0c527c34eaa8f9e14789
munirathinavel <munirathinavel@gmail.com>
2022-06-01 19:39:20 +0530

    Time: 12 ms, Memory: 54.6 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,24 +1,54 @@
-// Recursive Solution
+
 class Solution {
    
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> paths = new ArrayList<>();
-        dfs(graph, 0, new ArrayList<>(), paths);
+        List<List<Integer>> paths = new ArrayList();
+        if(graph == null || graph.length == 0) {
+            return paths;
+        }
+        Queue<List<Integer>> queue = new LinkedList<>();
+        List<Integer> path = new ArrayList();
+        path.add(0);
+        queue.add(path);
+        
+        while(!queue.isEmpty()) {
+            List<Integer> currentPath = queue.remove();
+            int current = currentPath.get(currentPath.size() - 1);
+            for(int next:graph[current]) {
+                List<Integer> tempPath = new ArrayList(currentPath);
+                tempPath.add(next);
+                if(next == graph.length -1) {
+                    paths.add(tempPath);
+                } else {
+                    queue.add(tempPath);
+                }
+            }
+        }
         return paths;
     }
-    
-    private void dfs(int[][] graph, int node, ArrayList<Integer> path, List<List<Integer>> paths) {
-        path.add(node);
-        if(node == graph.length -1) {
-            paths.add(new ArrayList(path));
-            return;
-        }
-        for(int adj:graph[node]) {
-            dfs(graph, adj, path, paths);
-            path.remove(path.size() -1);
-        }
-    }
 }
+
+// Recursive Solution
+// class Solution {
+   
+//     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+//         List<List<Integer>> paths = new ArrayList<>();
+//         dfs(graph, 0, new ArrayList<>(), paths);
+//         return paths;
+//     }
+    
+//     private void dfs(int[][] graph, int node, ArrayList<Integer> path, List<List<Integer>> paths) {
+//         path.add(node);
+//         if(node == graph.length -1) {
+//             paths.add(new ArrayList(path));
+//             return;
+//         }
+//         for(int adj:graph[node]) {
+//             dfs(graph, adj, path, paths);
+//             path.remove(path.size() -1);
+//         }
+//     }
+// }
 
 //  Iterative Approach with Stack & Queue
 // class Solution {
