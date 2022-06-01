582876fbd104892bc6bb446a3575bf02484a883a
munirathinavel <munirathinavel@gmail.com>
2022-06-01 18:45:22 +0530

    Time: 114 ms, Memory: 198.2 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,31 +1,40 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        
-        List<List<Integer>> graph = new ArrayList<>(n);
-        for(int i=0; i < n; i++) {
-            graph.add(new ArrayList<Integer>());
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        List<List<Integer>> adjacency_list = new ArrayList<>();        
+        for (int i = 0; i < n; i++) {
+            adjacency_list.add(new ArrayList<>());
         }
-        for(int[] edge:edges) {
-            graph.get(edge[0]).add(edge[1]); 
-            graph.get(edge[1]).add(edge[0]); 
-        }
-        Set<Integer> visited = new HashSet<>();
-        Queue<Integer> q = new LinkedList<>();
-        q.add(source);
-        visited.add(source);
         
-        while(!q.isEmpty()) {
-            int current = q.remove();
-            if(current == destination) {
+        for (int[] edge : edges) {
+            adjacency_list.get(edge[0]).add(edge[1]);
+            adjacency_list.get(edge[1]).add(edge[0]);
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        boolean seen[] = new boolean[n];
+        Arrays.fill(seen, false);
+        seen[start] = true;
+        
+        while (!queue.isEmpty()) {
+            // Get the current node.
+            int node = queue.poll();
+            
+            // Check if we have reached the target node.
+            if (node == end) {
                 return true;
             }
-            for(int neighbor: graph.get(current)) {
-                if(!visited.contains(neighbor)) {
-                    q.add(neighbor);
-                    visited.add(current);
+            
+            // Add all neighbors to the stack.
+            for (int neighbor : adjacency_list.get(node)) {
+                // Check if neighbor has been added to the queue before.
+                if (!seen[neighbor]) {
+                    seen[neighbor] = true;
+                    queue.add(neighbor);
                 }
             }
         }
+        
         return false;
     }
 }
