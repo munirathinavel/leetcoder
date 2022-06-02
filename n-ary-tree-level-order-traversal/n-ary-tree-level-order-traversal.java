0df2812a95b241494c222976ef76637b5f8a6ba6
munirathinavel <munirathinavel@gmail.com>
2022-06-02 07:16:27 +0530

    Time: 1 ms, Memory: 46.8 MB - LeetHub

--- n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.java
+++ n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.java
@@ -16,28 +16,55 @@ class Node {
     }
 };
 */
-
+// DFS
 class Solution {
+    
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> res = new ArrayList<>();
-        if(root == null) {
-            return res;
-        }
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        
-        while(!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> levelList = new ArrayList<>();
-            for(int i = 0; i < size; i++) {
-                Node current = q.poll();
-                levelList.add(current.val);
-                for(Node adj: current.children) {
-                    q.add(adj);
-                }
-            }
-            res.add(levelList);
+        if(root != null) {
+            dfs(root, 1, res);
         }
         return res;
     }
+    
+    private void dfs(Node root, int depth, List<List<Integer>> res) {
+        if(root == null) {
+            return;
+        }
+        if(res.size() < depth) {
+            ArrayList<Integer> row = new ArrayList<>();
+            row.add(root.val);
+            res.add(row);
+        } else {
+            res.get(depth-1).add(root.val);
+        }
+        for(Node next:root.children) {
+            dfs(next, depth+1, res);
+        }
+    }
 }
+// BFS with Queue
+// class Solution {
+//     public List<List<Integer>> levelOrder(Node root) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         if(root == null) {
+//             return res;
+//         }
+//         Queue<Node> q = new LinkedList<>();
+//         q.add(root);
+        
+//         while(!q.isEmpty()) {
+//             int size = q.size();
+//             List<Integer> levelList = new ArrayList<>();
+//             for(int i = 0; i < size; i++) {
+//                 Node current = q.poll();
+//                 levelList.add(current.val);
+//                 for(Node adj: current.children) {
+//                     q.add(adj);
+//                 }
+//             }
+//             res.add(levelList);
+//         }
+//         return res;
+//     }
+// }
