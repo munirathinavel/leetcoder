f17dd1ebe9c280f5a4b17cbea04ebd8c729cb1cc
munirathinavel <munirathinavel@gmail.com>
2022-06-05 21:01:03 +0530

    Time: 175 ms, Memory: 55 MB - LeetHub

--- minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
+++ minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
@@ -1,29 +1,50 @@
-class Solution {
+public class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0 ; i < list1.length; i++) {
-            String name = list1[i];
-            map.put(name, i);
-        }
-        int leastListIndexSum = Integer.MAX_VALUE;
-        Map<String, Integer> resMap = new HashMap<>();
-        for(int i = 0; i < list2.length; i++) {
-            String name = list2[i];
-            if(map.containsKey(name)) {
-                int indexSum = i+ map.get(name);
-                if(leastListIndexSum > indexSum) {
-                    leastListIndexSum = indexSum;
+        HashMap < Integer, List < String >> map = new HashMap < > ();
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j])) {
+                    if (!map.containsKey(i + j))
+                        map.put(i + j, new ArrayList < String > ());
+                    map.get(i + j).add(list1[i]);
                 }
-                resMap.put(name, indexSum);
             }
         }
-        List<String> res = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry: resMap.entrySet()){
-            if(entry.getValue() == leastListIndexSum) {
-                res.add(entry.getKey());
-            }
-        }
-        System.out.println(resMap);
-        return res.toArray(new String[0]);
+        int min_index_sum = Integer.MAX_VALUE;
+        for (int key: map.keySet())
+            min_index_sum = Math.min(min_index_sum, key);
+        String[] res = new String[map.get(min_index_sum).size()];
+        return map.get(min_index_sum).toArray(res);
     }
 }
+
+
+// class Solution {
+//     public String[] findRestaurant(String[] list1, String[] list2) {
+//         Map<String, Integer> map = new HashMap<>();
+//         for(int i=0 ; i < list1.length; i++) {
+//             String name = list1[i];
+//             map.put(name, i);
+//         }
+//         int leastListIndexSum = Integer.MAX_VALUE;
+//         Map<String, Integer> resMap = new HashMap<>();
+//         for(int i = 0; i < list2.length; i++) {
+//             String name = list2[i];
+//             if(map.containsKey(name)) {
+//                 int indexSum = i+ map.get(name);
+//                 if(leastListIndexSum > indexSum) {
+//                     leastListIndexSum = indexSum;
+//                 }
+//                 resMap.put(name, indexSum);
+//             }
+//         }
+//         List<String> res = new ArrayList<>();
+//         for(Map.Entry<String, Integer> entry: resMap.entrySet()){
+//             if(entry.getValue() == leastListIndexSum) {
+//                 res.add(entry.getKey());
+//             }
+//         }
+//         System.out.println(resMap);
+//         return res.toArray(new String[0]);
+//     }
+// }
