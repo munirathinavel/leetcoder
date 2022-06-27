54cea00d7c5015896b2472735cd1839b3279e91e
munirathinavel <munirathinavel@gmail.com>
2022-06-27 09:51:00 +0530

    Time: 0 ms, Memory: 40.6 MB - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.java
+++ n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,3 +1,4 @@
+// Bottom-up approach - Optimized
 class Solution {
     public int tribonacci(int n) {
         if(n == 0) {
@@ -6,13 +7,34 @@ class Solution {
             return 1;
         }
         
-        int dp[] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int one = 0;
+        int two = 1;
+        int three = 1;
         for(int i = 3; i < n + 1; i++) {
-            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+            int current = three + two + one;
+            one = two;
+            two = three;
+            three = current;
         }
-        return dp[n];
+        return three;
     }
 }
+// Bottom-up approach
+// class Solution {
+//     public int tribonacci(int n) {
+//         if(n == 0) {
+//             return 0;
+//         } else if(n < 3) {
+//             return 1;
+//         }
+        
+//         int dp[] = new int[n+1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         dp[2] = 1;
+//         for(int i = 3; i < n + 1; i++) {
+//             dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+//         }
+//         return dp[n];
+//     }
+// }
