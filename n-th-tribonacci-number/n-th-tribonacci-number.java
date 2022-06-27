734a139770c09aa6e534b7ceb21ab07ba636085b
munirathinavel <munirathinavel@gmail.com>
2022-06-27 09:54:16 +0530

    Time: 0 ms, Memory: 40.9 MB - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.java
+++ n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -29,7 +29,6 @@ class Solution {
 //         }
         
 //         int dp[] = new int[n+1];
-//         dp[0] = 0;
 //         dp[1] = 1;
 //         dp[2] = 1;
 //         for(int i = 3; i < n + 1; i++) {
