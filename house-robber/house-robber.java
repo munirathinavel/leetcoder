14371d470c6bd86087aaab040562615bf808f2b6
munirathinavel <munirathinavel@gmail.com>
2022-06-27 08:14:31 +0530

    Time: 0 ms, Memory: 41.9 MB - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,20 +1,37 @@
 // Top Down approach
 class Solution {
-    Map<Integer,Integer> map = new HashMap<>();
-    int[] nums;
     public int rob(int[] nums) {
-        this.nums = nums;
-        return dp(nums.length-1);
-    }
-    
-    private int dp(int i) {
-        if(i == 0) {
+        int len = nums.length;
+        if(len == 1) {
             return nums[0];
-        } else if(i == 1) {
-            return Math.max(nums[0], nums[1]);
-        } else if(!map.containsKey(i)) {
-            map.put(i, Math.max(dp(i-1), dp(i-2) + nums[i]));
         }
-        return map.get(i);
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+       for(int i = 2; i < nums.length; i++){
+           dp[i] = Math.max(dp[i-1], dp[i-2]+ nums[i]);
+       }
+        return dp[len-1];
     }
 }
+
+// Top Down approach
+// class Solution {
+//     Map<Integer,Integer> map = new HashMap<>();
+//     int[] nums;
+//     public int rob(int[] nums) {
+//         this.nums = nums;
+//         return dp(nums.length-1);
+//     }
+    
+//     private int dp(int i) {
+//         if(i == 0) {
+//             return nums[0];
+//         } else if(i == 1) {
+//             return Math.max(nums[0], nums[1]);
+//         } else if(!map.containsKey(i)) {
+//             map.put(i, Math.max(dp(i-1), dp(i-2) + nums[i]));
+//         }
+//         return map.get(i);
+//     }
+// }
