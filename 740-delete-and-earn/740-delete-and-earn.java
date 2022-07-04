38b853f452e13f69e4e05e6d50f3478f74a48468
munirathinavel <munirathinavel@gmail.com>
2022-07-04 07:25:50 +0530

    Time: 13 ms (51.17%), Space: 48 MB (48.51%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,5 +1,5 @@
+// Bottom-up approach
 class Solution {
-    Map<Integer, Integer> cache = new HashMap<>();
     Map<Integer, Integer> points = new HashMap<>();
     
     public int deleteAndEarn(int[] nums) {
@@ -8,21 +8,43 @@ class Solution {
             points.put(num, points.getOrDefault(num, 0) + num);
             maxNumber = Math.max(num, maxNumber);
         }
-        // Function call
-        return deleteEarn(maxNumber);
-    }
-    
-    private int deleteEarn(int num) {
-        // Base cases
-        if(num == 0) {
-            return 0;
-        } else if(num == 1) {
-            return points.getOrDefault(1, 0);
-        } else if(cache.containsKey(num)) {
-            return cache.get(num);
+        int[] maxPoints = new int[maxNumber + 1];
+        maxPoints[1] = points.getOrDefault(1, 0);
+        
+        for(int i = 2; i <= maxNumber; i++) {
+            maxPoints[i] = Math.max(maxPoints[i-1], maxPoints[i-2] + points.getOrDefault(i, 0));
         }
-        // Recurrence relation
-        cache.put(num, Math.max(deleteEarn(num - 1), deleteEarn(num - 2) + points.getOrDefault(num, 0)));
-        return cache.get(num);
+        
+        return maxPoints[maxNumber];
     }
 }
+
+// Top-down approach
+// class Solution {
+//     Map<Integer, Integer> cache = new HashMap<>();
+//     Map<Integer, Integer> points = new HashMap<>();
+    
+//     public int deleteAndEarn(int[] nums) {
+//         int maxNumber = 0;
+//         for(int num: nums) {
+//             points.put(num, points.getOrDefault(num, 0) + num);
+//             maxNumber = Math.max(num, maxNumber);
+//         }
+//         // Function call
+//         return deleteEarn(maxNumber);
+//     }
+    
+//     private int deleteEarn(int num) {
+//         // Base cases
+//         if(num == 0) {
+//             return 0;
+//         } else if(num == 1) {
+//             return points.getOrDefault(1, 0);
+//         } else if(cache.containsKey(num)) {
+//             return cache.get(num);
+//         }
+//         // Recurrence relation
+//         cache.put(num, Math.max(deleteEarn(num - 1), deleteEarn(num - 2) + points.getOrDefault(num, 0)));
+//         return cache.get(num);
+//     }
+// }
