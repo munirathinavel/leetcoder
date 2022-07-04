df19bcca3b281604299c2779fa384527e7a82943
munirathinavel <munirathinavel@gmail.com>
2022-07-04 08:09:32 +0530

    Time: 11 ms (60.65%), Space: 49 MB (26.94%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,31 +1,72 @@
-// Bottom-up approach space optimized
+// Bottom-up + Iterative approach space optimized
 class Solution {
-    Map<Integer, Integer> points = new HashMap<>();
-    
     public int deleteAndEarn(int[] nums) {
+        Map<Integer, Integer> points = new HashMap<>();
+        int maxNumber = 0;
         for(int num: nums) {
             points.put(num, points.getOrDefault(num, 0) + num);
+            maxNumber = Math.max(num, maxNumber);
         }
-        List<Integer> elements = new ArrayList<Integer>(points.keySet());
-        Collections.sort(elements);
         
-        int back1 = points.get(elements.get(0));
+        
+        int back1 = 0;
         int back2 = 0;
-        
-        for(int i = 1; i < elements.size(); i++) {
-            int current = elements.get(i);
-            int temp  = back1;
-            if(current == elements.get(i-1) + 1) {
-                back1 = Math.max(back1, back2 + points.get(current));
-            } else {
-                back1 += points.get(current);
+        int n = points.size();
+        if(maxNumber >= n + n * Math.log(n) / Math.log(2)) {
+            List<Integer> elements = new ArrayList<Integer>(points.keySet());
+            Collections.sort(elements);
+            back1 = points.get(elements.get(0));
+            for(int i = 1; i < elements.size(); i++) {
+                int current = elements.get(i);
+                int temp  = back1;
+                if(current == elements.get(i-1) + 1) {
+                    back1 = Math.max(back1, back2 + points.get(current));
+                } else {
+                    back1 += points.get(current);
+                }
+                back2 = temp;
             }
-            back2 = temp;
+        } else {
+            back1 = points.getOrDefault(1, 0);
+            for(int i=2; i <= maxNumber; i++) {
+                int temp = Math.max(back1, back2 + points.getOrDefault(i, 0));
+                back2 = back1;
+                back1 = temp;
+            }
         }
+        
         
         return back1;
     }
 }
+// Iterative approach space optimized
+// class Solution {
+//     Map<Integer, Integer> points = new HashMap<>();
+    
+//     public int deleteAndEarn(int[] nums) {
+//         for(int num: nums) {
+//             points.put(num, points.getOrDefault(num, 0) + num);
+//         }
+//         List<Integer> elements = new ArrayList<Integer>(points.keySet());
+//         Collections.sort(elements);
+        
+//         int back1 = points.get(elements.get(0));
+//         int back2 = 0;
+        
+//         for(int i = 1; i < elements.size(); i++) {
+//             int current = elements.get(i);
+//             int temp  = back1;
+//             if(current == elements.get(i-1) + 1) {
+//                 back1 = Math.max(back1, back2 + points.get(current));
+//             } else {
+//                 back1 += points.get(current);
+//             }
+//             back2 = temp;
+//         }
+        
+//         return back1;
+//     }
+// }
 
 // Bottom-up approach space optimized
 // class Solution {
