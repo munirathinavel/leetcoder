f07bef9bdb2b99ed62a9f568025ab7472c8a87c3
munirathinavel <munirathinavel@gmail.com>
2022-07-25 19:41:41 +0530

    Time: 12 ms (26.14%), Space: 57.3 MB (75.16%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,17 +1,42 @@
+// Space Optimized Solution
+
 class Solution {
     public int maximalSquare(char[][] matrix) {
         int rows = matrix.length;
         int cols = rows > 0 ? matrix[0].length : 0;
-        int[][] dp = new int[rows + 1][cols + 1];
-        int maxsqlen =0;
+        int[] dp = new int[cols + 1];
+        int maxsqlen = 0, prev = 0;
         for(int i = 1; i <= rows; i++) {
             for(int j = 1; j <= cols; j++) {
+                 int temp = dp[j];
                 if (matrix[i-1][j-1] == '1') {
-                    dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1;
-                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
+                    dp[j] = Math.min(Math.min(dp[j-1], dp[j]),prev) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[j]);
+                } else {
+                    dp[j] = 0;
                 }
+                 prev = temp;
             }
         }
         return maxsqlen * maxsqlen;
     }
 }
+
+// 2D DP Matrix Solution
+// class Solution {
+//     public int maximalSquare(char[][] matrix) {
+//         int rows = matrix.length;
+//         int cols = rows > 0 ? matrix[0].length : 0;
+//         int[][] dp = new int[rows + 1][cols + 1];
+//         int maxsqlen =0;
+//         for(int i = 1; i <= rows; i++) {
+//             for(int j = 1; j <= cols; j++) {
+//                 if (matrix[i-1][j-1] == '1') {
+//                     dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1;
+//                     maxsqlen = Math.max(maxsqlen, dp[i][j]);
+//                 }
+//             }
+//         }
+//         return maxsqlen * maxsqlen;
+//     }
+// }
