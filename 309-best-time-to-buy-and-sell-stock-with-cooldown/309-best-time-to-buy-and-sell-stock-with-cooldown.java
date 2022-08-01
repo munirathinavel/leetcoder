56291a64583d5c761c47f803bbbf19c5a5e1c875
munirathinavel <munirathinavel@gmail.com>
2022-08-01 19:57:38 +0530

    Time: 12 ms (13.86%), Space: 42.6 MB (32.87%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,15 +1,37 @@
+// Approach 2: Yet-Another Dynamic Programming
 class Solution {
     public int maxProfit(int[] prices) {
-        int sold = Integer.MIN_VALUE;
-        int held = Integer.MIN_VALUE;
-        int reset = 0;
+        int[] mp = new int[prices.length + 2];
         
-        for(int price: prices) {
-            int preSold = sold;
-            sold = held + price;
-            held = Math.max(held, reset-price);
-            reset = Math.max(reset, preSold);
+        for(int i = prices.length -1; i >= 0; i--) {
+            int c1 = 0;
+            // Case1 - buy & sell the stock
+            for(int sell = i+1; sell < prices.length; sell++) {
+                int profit = (prices[sell] - prices[i]) + mp[sell + 2];
+                c1 = Math.max(profit, c1);
+            }
+            // Case2 - do no transaction with stock p[i]
+            int c2 = mp[i + 1];
+
+            // wrap up the two cases
+            mp[i] = Math.max(c1, c2);
         }
-        return Math.max(sold, reset);
+        return mp[0];
     }
 }
+// Approach 1: Dynamic Programming with State Machine
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int sold = Integer.MIN_VALUE;
+//         int held = Integer.MIN_VALUE;
+//         int reset = 0;
+        
+//         for(int price: prices) {
+//             int preSold = sold;
+//             sold = held + price;
+//             held = Math.max(held, reset-price);
+//             reset = Math.max(reset, preSold);
+//         }
+//         return Math.max(sold, reset);
+//     }
+// }
