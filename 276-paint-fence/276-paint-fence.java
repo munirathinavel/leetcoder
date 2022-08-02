dc49b8757cd6e170f01fcca6821aa0c63ee40615
munirathinavel <munirathinavel@gmail.com>
2022-08-02 08:32:46 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (6.11%) - LeetHub

--- 276-paint-fence/276-paint-fence.java
+++ 276-paint-fence/276-paint-fence.java
@@ -1,19 +1,37 @@
+// Approach 2: Bottom-Up Dynamic Programming (Tabulation)
 class Solution {
     public int numWays(int n, int k) {
-        return totalWays(n, k);
-    }
-    
-    Map<Integer, Integer> memo = new HashMap<>();
-    
-    private int totalWays(int i, int k) {
-        if(i == 1) return k;
-        if(i == 2) return k * k;
+        if(n == 1) return k;
+        if(n == 2) return k*k;
         
-        if(memo.containsKey(i)) {
-            return memo.get(i);
+        int dp[] = new int[n + 1];
+        dp[0] = k;
+        dp[1] = k * k;
+        
+        for(int i = 2; i <= n; i++ ) {
+            dp[i] = (k-1) * (dp[i-1] + dp[i-2]);
         }
-        
-        memo.put(i, (k-1) * (totalWays(i-1, k) + totalWays(i-2, k)));
-        return memo.get(i);
+        return dp[n-1];
     }
 }
+
+// Approach 1: Top-Down Dynamic Programming (Recursion + Memoization)
+// class Solution {
+//     public int numWays(int n, int k) {
+//         return totalWays(n, k);
+//     }
+    
+//     Map<Integer, Integer> memo = new HashMap<>();
+    
+//     private int totalWays(int i, int k) {
+//         if(i == 1) return k;
+//         if(i == 2) return k * k;
+        
+//         if(memo.containsKey(i)) {
+//             return memo.get(i);
+//         }
+        
+//         memo.put(i, (k-1) * (totalWays(i-1, k) + totalWays(i-2, k)));
+//         return memo.get(i);
+//     }
+// }
